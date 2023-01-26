aeeed5b7abc8f793ae6332949526f25c2dc7cc90
shellyalmo <shellyalmo@gmail.com>
2023-01-26 18:05:30 +0200

    feat: update current score after dice roll click

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ const secondDiceImg = document.getElementById("dice-2");
 // new game button element
 const rollDiceBtn = document.getElementById("roll-dice-btn");
 
+// current scores elements
+const currentScorePlayer1 = document.getElementById("current-score-player1");
+const currentScorePlayer2 = document.getElementById("current-score-player2");
+
 /*functions*/
 
 /**
@@ -41,12 +45,29 @@ function rollDice() {
   dice[1] = randomNumber();
 }
 
+/**
+ * updates current score of each player according to the dice result at each dice roll
+ *@param {Array} lastDiceResults
+ */
+function updateCurrentScore(lastDiceResults) {
+  if (player1.isPlayingNow) {
+    player1.currentScore += dice[0] + dice[1];
+  } else if (player2.isPlayingNow) {
+    player2.currentScore += dice[0] + dice[1];
+  }
+}
+
 // if the player rolls a double six all his round’s score
 // gets lost.After that, it's the next player’s turn.
 
 /*event listeners*/
+
+//all events of roll dice button
 rollDiceBtn.addEventListener("click", function (e) {
   rollDice();
   firstDiceImg.setAttribute("src", `./assets/dice-${dice[0]}.png`);
   secondDiceImg.setAttribute("src", `./assets/dice-${dice[1]}.png`);
+  updateCurrentScore(dice);
+  currentScorePlayer1.innerText = player1.currentScore;
+  currentScorePlayer2.innerText = player2.currentScore;
 });
