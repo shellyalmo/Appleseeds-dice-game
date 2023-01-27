923f16ad8fb667cc1345f20228e263e07de6db99
shellyalmo <shellyalmo@gmail.com>
2023-01-27 15:41:13 +0200

    fix: page cleans after new game btn is clicked

--- modal.js
+++ modal.js
@@ -10,7 +10,8 @@ const startGameBtn = document.getElementById("start-game-btn");
 
 // When the user clicks the button, open the modal
 newGameBtn.onclick = function () {
-  modal.style.display = "block";
+  //refresh the page when starting a new game
+  window.location.reload();
 };
 
 // When the user clicks on start game button, close the modal
@@ -22,24 +23,4 @@ startGameBtn.onclick = function () {
 startGameBtn.addEventListener("click", function (e) {
   //initialize the target value from the user input
   targetScore = document.getElementById("targetscore").valueAsNumber;
-  // players objects initialized
-  let player1 = {
-    totalScore: 0,
-    currentScore: 0,
-    isPlayingNow: true,
-    isWinner: false,
-  };
-  let player2 = {
-    totalScore: 0,
-    currentScore: 0,
-    isPlayingNow: false,
-    isWinner: false,
-  };
-
-  //dice saved as an array
-  let dice = [0, 0];
-
-  //activate the game buttons
-  document.getElementById("roll-dice-btn").disabled = false;
-  document.getElementById("hold-btn").disabled = false;
 });
